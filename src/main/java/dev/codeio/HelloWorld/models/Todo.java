24967f88d9aaf6960c0f1521cd0c1446fd1de39e
TamilarasiG3 <your-email@example.com>
2026-07-10 21:29:45 +0530

    Update Todo.java

--- src/main/java/dev/codeio/HelloWorld/models/Todo.java
+++ src/main/java/dev/codeio/HelloWorld/models/Todo.java
@@ -1,43 +1,23 @@
 package dev.codeio.HelloWorld.models;
 
-
-
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
-//import jakarta.validation.constraints.NotBlank;
-//import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-//import lombok.Getter;
-//import lombok.Setter;
-//import org.hibernate.annotations.processing.Pattern;
 
 @Entity
-//@Getter
-//@Setter
-//@AllArgsConstructor
-//@NoArgsConstructor
-@Data   //For all annotations like getter,setter,constructor,etc,...
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Todo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    private Long id;
 
-//    @NotBlank
-//    @Pattern(regexp = "^[0-9]{10}$")
-//    @Size(min = 5, max = 15)
-    String title;
+    private String title;
 
-//    @NotBlank
-//    @Min(1)
-//    @Max(10)
-    @Schema(name = "title", example = "Complete Spring Boot")
-    String description;
+    private String description;
 
-   // @NotNull
-    Boolean isCompleted;
-
-
+    private Boolean isCompleted;
 }
